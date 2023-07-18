4cdd57a24edd29f60c12e7a0e86cebc6ca49bfda
lgweida <45736060+lgweida@users.noreply.github.com>
2023-07-17 21:08:08 -0400

    Update urls.py

--- django_finviz/urls.py
+++ django_finviz/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
 from django.urls import path, include
 
 urlpatterns = [
@@ -22,3 +24,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('', include('frontend.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns() # new
